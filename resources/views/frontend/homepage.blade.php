e12419081e919677c65809330b889774b9cf97d1
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-05-11 14:32:15 +0700

    dsa

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -106,20 +106,10 @@
                         <li class="nav-item">
                             <a class="nav-link active"
                             data-bs-toggle="pill"
-                            href="#tab-1">
-                            All
+                            href="{{ route('shop') }}">
+                            See all products
                             </a>
                         </li>
-                        @foreach($categoriesName as $category)
-                            <li class="nav-item">
-                            <a class="nav-link"
-                                data-bs-toggle="pill"
-                                href="#tab-{{ $category->slug }}">
-                                {{ $category->name }}
-                            </a>
-                            </li>
-                        @endforeach
-                        </ul>
                 </div>
             </div>
             <div class="tab-content">
@@ -157,42 +147,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                @foreach($categoriesName as $category)
-                    <div
-                        id="tab-{{ $category->slug }}"
-                        class="tab-pane fade {{ $loop->first ? 'show active' : '' }} p-0">
-                        <div class="row g-4">
-                            @foreach($category->where('slug', $category->slug)->products as $row)
-                                <div class="col-md-6 col-lg-4 col-xl-3">
-                                    <div class="rounded position-relative fruite-item">
-                                        <div class="fruite-img">
-                                            @php
-                                            $image = !empty($row->products->productImages->first()) ? asset('storage/'.$row->products->productImages->first()->path) : asset('images/placeholder.jpg');
-                                            @endphp
-                                            <img src="{{ $image }}" class="img-fluid w-100 rounded-top" alt="">
-                                        </div>
-                                        <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">{{ $row->categories->name }}</div>
-                                        <div class="p-3 border border-secondary border-top-0 rounded-bottom">
-                                            <a href="{{ route('shop-detail', $row->products->id) }}"><h4>{{ $row->products->name }}</h4></a>
-                                            <b>{{ $row->products->short_description }}</b>
-                                            @if ($row->products->productInventory != null)
-                                                <p>Stok : {{ $row->products->productInventory->qty }}</p>
-                                            @endif
-                                            <div class="d-flex justify-content-center flex-lg-wrap">
-                                                <p class="text-dark fs-5 fw-bold mb-2">Rp. {{ number_format($row->products->price) }}</p>
-                                                <a class="btn border add-to-card border-secondary rounded-pill px-3 text-primary" href="" product-id="{{ $row->products->id }}" product-type="{{ $row->products->type }}" product-slug="{{ $row->products->slug }}">
-                                                    <i class="fa fa-shopping-bag me-2 text-primary"></i>
-                                                    Add to cart
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                @endforeach
                 </div>
         </div>
     </div>
