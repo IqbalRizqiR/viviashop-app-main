02b8d12626158be981e063ec967a8cbd17fc850e
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-05-11 14:10:28 +0700

    dh

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -129,7 +129,7 @@
                 class="tab-pane fade show active">
                 <div class="row g-4">
                 @foreach($products as $row)
-                    @include('frontend.partials.frontend.product-card', ['product'=>$row])
+                    @include('frontend.partials.frontend.product-card', ['product' => $row])
                 @endforeach
                 </div>
             </div>
@@ -140,7 +140,7 @@
                     class="tab-pane fade">
                 <div class="row g-4">
                     @foreach($products->where('category_id',$category->id) as $row)
-                    @include('frontend.partials.frontend.product-card', ['product'=>$row])
+                        @include('frontend.partials.frontend.product-card', ['product'=>$row])
                     @endforeach
                 </div>
                 </div>
