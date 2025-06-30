b358c6c64bd2f40dcc3af7c4e21acec6cb847bc6
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-06-30 13:58:12 +0700

    ads

--- app/Imports/ProdukImport.php
+++ app/Imports/ProdukImport.php
@@ -21,6 +21,7 @@ class ProdukImport implements ToCollection, WithHeadingRow
     {
         // dd($rows);
         foreach ($rows as $row) {
+            dd($row);
             $products = Product::create([
                 'sku' => $row['name'], // jika sku tidak ada, gunakan nama
                 'type' => 'simple',
