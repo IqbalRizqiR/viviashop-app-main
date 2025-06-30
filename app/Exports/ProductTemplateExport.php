46805247d97e2fa35f56c3729c8ce24a454c290d
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-06-30 13:32:20 +0700

    dasdad

--- app/Exports/ProductTemplateExport.php
+++ app/Exports/ProductTemplateExport.php
@@ -1,75 +1,16 @@
 <?php
-
 namespace App\Exports;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithEvents;
-use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
 
 use App\Models\Category;
-use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ProductTemplateExport implements FromCollection, WithHeadings, WithEvents
+class ProductTemplateExport implements WithMultipleSheets
 {
-    public function collection()
-    {
-        // Bisa kosong, karena hanya ingin buat template
-        return collect([
-            [
-                'name' => '',
-                'price' => '',
-                'harga_beli' => '',
-                'category_id' => '', // dropdown isi nama category
-                'short_description' => '',
-                'description' => '',
-                'sku' => '',
-                'stok' => '',
-            ],
-        ]);
-    }
-
-    public function headings(): array
+    public function sheets(): array
     {
         return [
-            'name',
-            'price',
-            'harga_beli',
-            'category_id', // dropdown isi nama category
-            'short_description',
-            'description',
-            'sku',
-            'stok',
-
-        ];
-    }
-
-    public function registerEvents(): array
-    {
-        return [
-            AfterSheet::class => function (AfterSheet $event) {
-                $sheet = $event->sheet->getDelegate();
-
-                $categories = Category::pluck('name')->toArray();
-
-                // Handle tanda kutip agar tidak crash di formula Excel
-                $list = implode(',', array_map(function ($v) {
-                    return '"' . str_replace('"', '""', $v) . '"';
-                }, $categories));
-
-                // Set dropdown untuk baris 2-100 di kolom C (category_id)
-                for ($row = 2; $row <= 100; $row++) {
-                    $validation = $sheet->getCell("C$row")->getDataValidation();
-                    $validation->setType(DataValidation::TYPE_LIST);
-                    $validation->setErrorStyle(DataValidation::STYLE_STOP);
-                    $validation->setAllowBlank(true);
-                    $validation->setShowInputMessage(true);
-                    $validation->setShowErrorMessage(true);
-                    $validation->setShowDropDown(true);
-                    $validation->setFormula1('"' . $list . '"');
-                }
-            }
+            new ProductSheetExport(),
+            new CategorySheetExport(),
         ];
     }
 }
-
-?>
