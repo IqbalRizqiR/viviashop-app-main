5009cd74f6155683420646d171b18a4a63829f00
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-06-30 09:35:47 +0700

    asddsa

--- resources/views/frontend/partials/user_menu.blade.php
+++ resources/views/frontend/partials/user_menu.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar-widget mb-45 d-flex flex-column justify-content-center align-items-center" style="margin-top: 130px;">
-	<h3 class="sidebar-title text-center" style="">User Menu</h3>
-	<div class="sidebar-categories">
-		<ol class="text-center">
+    <div class="sidebar-categories">
+        <ol class="text-center">
+            <h3 class="sidebar-title text-center" style="">User Menu</h3>
 			<h6><a style="color: black;" href="{{ url('profile') }}">Profile</a></h6>
 			<h6><a style="color: black;" href="{{ url('orders') }}">Orders</a></h6>
 			<h6><a style="color: black;" href="{{ url('carts') }}">Cart</a></h6>
