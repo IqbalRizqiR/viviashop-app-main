3e6035d9a6897e6828a7be1b2e28feb26fea0846
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-06-30 09:34:48 +0700

    adsadsa

--- resources/views/frontend/partials/user_menu.blade.php
+++ resources/views/frontend/partials/user_menu.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar-widget mb-45 d-flex flex-column justify-content-center align-items-center" style="margin-top: 130px;">
-	<h3 class="sidebar-title" style="">User Menu</h3>
+	<h3 class="sidebar-title text-center" style="">User Menu</h3>
 	<div class="sidebar-categories">
 		<ol class="text-center">
 			<h6><a style="color: black;" href="{{ url('profile') }}">Profile</a></h6>
