c77ba1a75ea29ce8f2323feddcee849aafed721b
IqbalRizqiR <rama.ramajr121@gmail.com>
2025-09-19 16:37:05 +0700

    asd

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -31,7 +31,7 @@ class PembelianController extends Controller
         // dd($pembelian_detail);
 
         $pdf  = Pdf::loadView('admin.orders.invoicesBeli', compact('pembelian', 'pembelian_detail'))->setOptions(['defaultFont' => 'sans-serif']);
-        $customPaper = array(0, 0, 800, 111.386);
+        $customPaper = array(0, 0, 2, 2);
         $pdf->setPaper($customPaper);
         return $pdf->stream('invoice.pdf');
     }
